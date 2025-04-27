aea8bc90413f69031a432312b2f47a040cbeff93
Isaac Aires <playerredbrasil@gmail.com>
2025-04-27 15:04:43 -0300

    Ajustes no Dockerfile e docker-compose.yml

--- RO.DevTest/RO.DevTest.Tests/obj/Debug/net8.0/RO.DevTest.Tests.AssemblyInfo.cs
+++ RO.DevTest/RO.DevTest.Tests/obj/Debug/net8.0/RO.DevTest.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RO.DevTest.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f6c5cfe9e36f6a39cf546f7bc4be872ac46197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d1af6520785be2d724f8c4026735842807178d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RO.DevTest.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RO.DevTest.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
